63ce62f2c654b6d679a5bdfc78995e367d548dd5
haya hide <hayahdie23@gmail.com>
2024-12-01 16:04:50 +0900

    test1

--- src/App.js
+++ src/App.js
@@ -7,16 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <h1>Hello World</h1>
+          this app is Amplify at react.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
       </header>
     </div>
   );
