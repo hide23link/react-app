aa80b2feb5a94c2030038af0f34fcae5ed6d0cb4
haya hide <hayahdie23@gmail.com>
2024-12-01 21:32:12 +0900

    deploy2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <h1>Hello World</h1>
+          <h1>Fease 2</h1>
           this app is Amplify at react.
         </p>
        
